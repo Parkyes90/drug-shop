75940e32f038ec12c2b929f2174ded94d2567e29
YeseungPark <parkyes90@gmail.com>
2021-01-03 22:07:30 +0900

    feat(drug-shop): Add order, filter to users schema

--- server/users/schema.py
+++ server/users/schema.py
@@ -1,19 +1,43 @@
 import graphene
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django_filters import FilterSet, OrderingFilter
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 
+class UserFilter(FilterSet):
+    class Meta:
+        model = get_user_model()
+        fields = {
+            "username": ["icontains"],
+        }
+
+    order_by = OrderingFilter(fields=(("id", "id"), ("username", "username")))
+
+
 class UserNode(DjangoObjectType):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email")
-        filter_fields = {
-            "username": ["icontains"],
-        }
+        fields = ("id", "username", "email", "groups")
+        filterset_class = UserFilter
+
+        interfaces = (relay.Node,)
+
+
+class GroupNode(DjangoObjectType):
+    class Meta:
+        model = Group
+        fields = ("id", "name")
         interfaces = (relay.Node,)
 
 
 class Query(graphene.ObjectType):
-    all_users = DjangoFilterConnectionField(UserNode)
+    user = relay.Node.Field(UserNode)
+    users = DjangoFilterConnectionField(UserNode)
+
+    group = relay.Node.Field(GroupNode)
+
+    def resolve_users(self, info, **kwargs):
+        return get_user_model().objects.prefetch_related("groups")
